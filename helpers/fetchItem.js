c30703aa35385900819ce39fb0bcd3fd18221865
Afra Taiza <afrataiza@gmail.com>
2022-03-27 16:58:11 -0300

    Adiciona resoluçao do requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  if (item === undefined) {
+    throw new Error('You must provide an url');
+   } else {
+    const url = `https://api.mercadolibre.com/items/${item}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,34 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se fetchItem é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchItem).toBe('function');
+  });
+ 
+  test('Execute a função fetchItem com o argumento "MLB1615760527" e teste se fetch foi chamada', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  
+  test('Teste se, ao chamar a função fetchItem com o argumento "MLB1615760527", a função fetch utiliza o endpoint correto', async () => {
+    expect.assertions(1);
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527'
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+ 
+  test('Teste se o retorno da função fetchItem com o argumento "MLB1615760527" é uma estrutura de dados igual ao objeto item', async () => {
+    expect.assertions(1);
+    const response = await fetchItem('MLB1615760527')
+    expect(response).toEqual(item);
+  });
+ 
+  test('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    expect.assertions(1);
+    await expect(fetchItem()).rejects.toThrow('You must provide an url');
+  });
 });
+
+ 
